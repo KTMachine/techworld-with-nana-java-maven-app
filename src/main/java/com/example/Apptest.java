3743d04eb2c30be6d8f9a94a96ecf5eba373d56d
KTMachine <maximus_clarke@yahoo.com>
2025-02-03 14:24:21 -0300

    Update Apptest.java

--- src/main/java/com/example/Apptest.java
+++ src/main/java/com/example/Apptest.java
@@ -1,16 +1,1 @@
-import com.example.Application;
-import org.junit.Test;
-import static org.junit.Asser.*;
 
-public class AppTest {
-
-    @Test
-    public void testApp() {
-        Application myApp = new Application();
-
-        String result = myApp.getStatus();
-
-        assertEquals("OK", result);
-    }
-
-}
